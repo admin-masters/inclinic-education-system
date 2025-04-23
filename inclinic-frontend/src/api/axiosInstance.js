fb2e5dadb7b60e4223fc2bdbc6551fcb3c42601c
admin-masters <github.master@inditech.co.in>
2025-04-23 18:53:33 +0530

    Delete inclinic-frontend directory

--- inclinic-frontend/src/api/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
-  withCredentials: true, // if using cookies/session from Django
-});
-
-export default axiosInstance;
