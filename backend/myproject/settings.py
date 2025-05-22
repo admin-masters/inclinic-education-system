2189c47e3d3418504f54b785bc8edf0f177dc80f
bharti78 <2023kucp1049@iiitkota.ac.in>
2025-05-22 11:25:39 +0530

    first commit

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-dev')  # Keep secret in production
 
-DEBUG = True  # For development only
+DEBUG = False  # For development only
 
 ALLOWED_HOSTS = [
     'new.cpdinclinic.co.in',
